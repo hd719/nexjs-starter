22f13cf3b2caa9f1ca8a1a65b59680b34ffea540
Hamel Desai <hameldesai93@gmail.com>
2024-05-06 19:25:02 -0400

    section-1-video-5

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 // import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link href="/performance">Scale</Link>
+      <br />
+      <Link href="/performance">Performance</Link>
+      <div>Home</div>
+    </div>
+  );
 }
